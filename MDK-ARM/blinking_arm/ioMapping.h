5cca50480e1b3a64b956aa8728a2a32789f7d573
Yves <raphael.bastos@me.com>
2017-04-13 22:18:38 -0300

    Initial commit

---
+++ MDK-ARM/blinking_arm/ioMapping.h
@@ -0,0 +1,14 @@
+// Criar arquivo ioMapping.h com os pinos e registradores que serão usados
+//Pinos
+
+
+#ifndef IOMAPPING_H
+#define IOMAPPING_H
+
+
+#define BLINKING_LED                ((uint16_t)0x2000)  /* Pin 13 selected   */
+
+//Registradores
+
+
+#endif
