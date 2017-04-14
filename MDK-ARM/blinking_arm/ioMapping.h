f4718c314ccbb685a1d44c2e28a50410d41f1e94
Yves <raphael.bastos@me.com>
2017-04-13 22:37:15 -0300

    Incremented ioMapping.h

--- MDK-ARM/blinking_arm/ioMapping.h
+++ MDK-ARM/blinking_arm/ioMapping.h
@@ -2,13 +2,24 @@
 //Pinos
 
 
-#ifndef IOMAPPING_H
-#define IOMAPPING_H
-
 
 #define BLINKING_LED                ((uint16_t)0x2000)  /* Pin 13 selected   */
+#define PERIPHERAL_BASE				((uint32_t)0x40000000) 
+#define APB2PERIPHERAL_BASE			((PERIPHERAL_BASE + 0x10000))
+#define GPIOC_BASE					((APB2PERIPHERAL_BASE + 0x10000))
+#define GPIOC 						((GPIO *) GPIOC_BASE)
 
+typedef struct
+{
+  __IO uint32_t CRL;
+  __IO uint32_t CRH;
+  __IO uint32_t IDR;
+  __IO uint32_t ODR;
+  __IO uint32_t BSRR;
+  __IO uint32_t BRR;
+  __IO uint32_t LCKR;
+} GPIO;
 //Registradores
 
 
-#endif
+
